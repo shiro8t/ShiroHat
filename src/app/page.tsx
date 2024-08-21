c869b7d4d119d1d432f172b122203046bea75de0
shirohat <m.shirohat@gmail.com>
2024-08-21 23:05:05 +0900

    Update Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import { getSortedPostsData } from '@/lib/posts';
 import Link from 'next/link';
+import { FiArrowLeft } from 'react-icons/fi';
 
 export default async function Home() {
   const allPostsData = getSortedPostsData();
 
   return (
-    <section className="max-w-2xl mx-auto my-10">
-      <h1 className="text-4xl font-bold mb-5">shirohat&apos;s Blog</h1>
-      <ul className="space-y-5">
-        {allPostsData.map(({ id, date, title }) => (
-          <li key={id} className="border-b border-gray-200 pb-2.5">
-            <Link href={`/posts/${id}`} className="text-xl font-semibold text-[#007bff] hover:underline">
-              {title}
-            </Link>
-            <br />
-            <small className="text-gray-400">{date}</small>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <div className="max-w-2xl mx-auto my-10">
+      <section>
+        <h1 className="text-4xl font-bold mb-5">ShiroHat</h1>
+        <ul className="space-y-5">
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id} className="border-b border-gray-200 pb-2.5">
+              <Link href={`/posts/${id}`} className="text-xl font-semibold text-[#007bff] hover:underline">
+                {title}
+              </Link>
+              <br />
+              <small className="text-gray-400">{date}</small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   );
 }
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { getPostData } from '@/lib/posts';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import { FiArrowLeft } from 'react-icons/fi';
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const postData = await getPostData(params.id);
@@ -10,9 +12,22 @@ export default async function Post({ params }: { params: { id: string } }) {
   const postData = await getPostData(params.id);
 
   return (
-    <article className="max-w-2xl mx-auto my-10">
-      <h1 className="text-4xl font-bold mb-5">{postData.title}</h1>
-      <div className="custom-markdown" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
+    <div className="max-w-4xl mx-5 md:mx-auto my-10">
+      <article className="bg-white text-gray-800 rounded-lg overflow-hidden shadow-2xl">
+        <div className="select-none bg-gradient-to-r from-purple-100 via-pink-100 to-red-100 h-40 flex items-center justify-center text-6xl space-x-4">
+          <p>🌟</p>
+          <p>✨</p>
+          <p>🚀</p>
+        </div>
+        <div className="p-[32px] sm:p-[50px] space-y-[32px]">
+          <h1 className="text-5xl font-extrabold leading-tight">{postData.title}</h1>
+          <div className="prose lg:prose-xl prose-gray max-w-none custom-markdown" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </div>
+        <div className="bg-gray-50 px-[32px] py-5 border-t border-gray-200 flex items-center justify-between">
+          <Link href="/" className="text-blue-500 flex items-center space-x-0.5 rounded-full hover:bg-blue-100 px-2.5 py-0.5"><FiArrowLeft /><p className="hidden md:block">Back to home</p></Link>
+          <span className="text-gray-500 text-sm">Published on {new Date().toLocaleDateString()}</span>
+        </div>
+      </article>
+    </div>
   );
 }
